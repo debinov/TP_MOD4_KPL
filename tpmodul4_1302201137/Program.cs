01b9ea213b5e050a088d4e0296b7033bd39d1f73
debinov <debinv4nt@gmail.com>
2022-03-18 16:26:09 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302201137/Program.cs
+++ tpmodul4_1302201137/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+//Debi Ratnasari Novianti 1302201137 SE-44-03
+namespace tpmod4_1302201137
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGeneric<String> data = new DataGeneric<string>(" 1302201137 ");
+            data.PrintData();
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data Yang Tersimpan Adalah NIM : " + this.data);
+        }
+    }
+}
